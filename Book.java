429caeb4f1c8c55a8029c99ece2107ff84b2464e
salehhd123 <71578301+salehhd123@users.noreply.github.com>
2023-08-02 14:23:32 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,23 @@
+public class Book extends Product{
+    private String author;
+    public  Book(){}
+
+    public Book(String name, double price, String author) {
+        super(name, price);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public double getDiscount() {
+    double disc =getPrice()*0.5;
+        return getPrice()-disc;
+    }
+}
---
+++ Movie.java
@@ -0,0 +1,24 @@
+public class Movie extends Product{
+
+    private String director;
+    public Movie(){}
+
+    public Movie(String name, double price, String director) {
+        super(name, price);
+        this.director = director;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public double getDiscount() {
+        double disc =getPrice()*0.25;
+        return getPrice()-disc;
+    }
+}
